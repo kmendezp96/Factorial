e6bf6ab27af5dab868745e3e7af9e3138de2b321
kmendezp96 <kmendezp@unal.edu.co>
2016-09-26 12:34:59 -0500

    Add files via upload
    Hace el factorial mediante un ciclo for

---
+++ Factorial.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorial;
+
+import java.util.Scanner;
+
+class Factorial{
+  public static int operar (int a){
+    int res=1;
+    for (int i=1;i<=a;i++){
+      res= res*i;
+    }
+    return res;
+  }
+  public static void main(String[] args) {
+        Scanner sc2 = new Scanner(System.in);
+        int a = sc2.nextInt();
+        System.out.print(operar(a));
+    }
+
+}
